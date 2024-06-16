17ca045b6643f813154fbb7429305d803cb826cb
Eduardo Ramirez <eduardoramirez@MacBook-Air-de-Eduardo.local>
2024-06-16 14:11:57 -0500

    update

---
+++ semana02/dia01-javascript-basico/main.js
@@ -0,0 +1,70 @@
+/* console.log('Hola Javascript! 3') */
+
+const numero1 = 20
+const numero2 = 20.50
+const numero3 = -36
+
+console.log(numero1)
+console.log(numero2)
+console.log(numero3)
+
+console.log(typeof numero1)
+console.log(typeof numero2)
+console.log(typeof numero3)
+
+console.log(1%2)
+console.log(2**3)
+console.log(Math.pow(2,3))
+
+console.log( 1 == 1) //true
+console.log(1 == '1') //true
+console.log(1 != '1') //false
+
+console.log( 1 === 1) //true
+console.log(1 === '1') //false ✅
+console.log(1 !== '1') //false✅
+
+console.log( true && false) //false
+console.log( true || false) //true
+console.log(!false) // NEGACIÓN 
+
+const cadena1 = 'codigo'
+const cadena2 = 'cadaga7'
+
+console.log(cadena1.length)
+console.log(cadena2.length)
+
+console.log(cadena1.length === cadena2.length) // false
+
+// 2. Retornar true si un numero es menor que 40 sino devolver false
+// 3. Retornar True si un numero es menor que 60 sino devolver False
+// 4. Retornar True si un numero es par sino devolver False
+// 5. Retornar True si un numero es impar sino devolver False
+// 6. calcular el area de un triangulo y mostrar su resultado si tenemos los valores base y altura
+// 7. (TODO) Almacenar en una constante un número de 3 cifras y mostrar la suma de sus cifras elevada al cubo.
+// 8. (TODO) Almacenar en una constante un monto de dinero, luego mostrar cuanto le toca a cada socio según la siguiente tabla:
+// SOCIO A = 30%, SOCIO B = 20%, SOCIO C = 60%
+
+// CONDICIONALES (IF)
+
+if (true) {
+    // El bloque que se ejecuta si la condición es verdadera (true)
+  }
+
+  const numero = 33
+
+  const esPar = numero % 2 === 0
+
+if(esPar) {
+    console.log('El número es par')
+}
+
+// CONDICIONALES (IF , ELSE)
+
+if(true){}
+
+
+
+
+
+
