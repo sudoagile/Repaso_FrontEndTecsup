8b100e56ab89dd1e53e8fc80b76fee7d9900cbc9
Eduardo Ramirez <eduardoramirez@MacBook-Air-de-Eduardo.local>
2024-06-23 23:55:31 -0500

    update

---
+++ semana03/dial-react-intro/src/App.jsx
@@ -0,0 +1,27 @@
+/* function App(){
+  return <h1>Hola React.js</h1>
+}
+
+export default App */
+
+//Componente usando ARROW FUNCTIONS
+
+// const App = () => {
+//   return(
+//     <h1>Hola React con arrow funtions</h1>
+//   )
+// }
+// componentes de REACT usando multiples líneas con su ELEMENTO PADER
+
+const App = () => {
+   return (
+    <section>
+      <h1>Hola React!</h1> 
+      <p>Estoy en el bootcamp!</p> 
+    </section>
+   )
+  }
+  //     <h1>Hola React con arrow funtions</h1>
+  //   )
+
+export default App
